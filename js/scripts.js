97889e83004b71737ebae0a97f3f267d43649052
bibhutighimire <ghimirebibhuti@gmail.com>
2020-07-22 18:06:53 -0600

    layout os done...i am working on edit which is not complete yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,96 @@
-const ul = document.querySelector("ul");
-let input = document.querySelector("input");
-const submitButton = document.querySelector("#btnAdd");
+const inputContentTitle = document.querySelector("#inputContentTitle");
+const inputContent = document.querySelector("#inputContent");
+const btnSubmitContent = document.querySelector("#btnSubmitContent");
+const btnClearContent = document.querySelector("#btnClearContent");
 
-submitButton.addEventListener("click", () => {
-    if (input.value.trim() !== "") {
-        let li = document.createElement("li");
-        li.innerText = input.value.trim();
-        ul.appendChild(li);
-        input.value = "";
+btnClearContent.addEventListener("click", Clear)
 
-        let completeBtn = document.createElement("a");
-        completeBtn.setAttribute("href", "#");
-        completeBtn.setAttribute("id", "completeBtnID");
-        completeBtn.innerText = "COMPLETE";
-        li.appendChild(completeBtn);
+function Clear() {
+    inputContentTitle.value = "";
+    inputContent.value = "";
+}
 
-        let deleteBtn = document.createElement("a");
-        deleteBtn.setAttribute("href", "#");
-        deleteBtn.setAttribute("id", "deleteBtnID");
-        deleteBtn.innerText = "DELETE";
-        li.appendChild(deleteBtn);
+class Article {
 
-        let editBtn = document.createElement("a");
-        editBtn.setAttribute("href", "#");
-        editBtn.setAttribute("id", "editBtnID");
-        editBtn.innerText = "EDIT";
-        li.appendChild(editBtn);
-
-
-        deleteBtn.onclick = function () {
-            let nodeToBeDeleted = document.getElementById("deleteBtnID");
-            let delThis = this.parentElement;
-            delThis.style.display = "none";
-        }
-
-        editBtn.onclick = function () {
-            let nodeToBeDeleted = document.getElementById("editBtn");
-            let editThis = this.parentElement;
-            console.log(editThis.firstChild);
-            var editInput=document.createElement('input');
-            console.log(editInput);
-            editInput.setAttribute("type","text");
-            editInput.setAttribute("placeholder","enter");
-            editInput.appendChild(editThis.firstChild);
-           
-           //editInput.innerText= tobeedited.innerText;
-        }
+    // We can use constructors to set the base properties for our objects (name / age / hobbies in this case.)
+    constructor(title = inputContentTitle.value, content = inputContent.value) // Default values set in our parameters.
+    {
+        this.title = title;
+        this.content = content;
     }
-    
+    // Creating method to add article's title and content
+    addArticle(inputContentTitle = "", inputContent = "") {
+        let ul = document.querySelector("ul");
+        let li = document.createElement("li");
+        ul.appendChild(li);
+        // Creating an element.
+        const titleOfContent = document.createElement("h2");
+        const bodyOfContent = document.createElement("P");
+        const dateTimeNow = document.createElement("time");
+        dateTimeNow.setAttribute("id", "showDateTime");
+
+        const today = new Date();
+
+        dateTimeNow.innerText = ("Created on: " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " , " + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate());
+
+        const btnDelete = document.createElement("button");
+        btnDelete.setAttribute("id", "btnStyle");
+        btnDelete.innerText = "Delete";
+        const btnEdit = document.createElement("button");
+        btnEdit.setAttribute("id", "btnStyle");
+        btnEdit.innerText = "Edit";
+        // Fill in the text of the element (using template literal.)
+        titleOfContent.textContent = `${this.title}`;
+        bodyOfContent.textContent = `${this.content}`;
+        // Add the new element to the body of our webpage.
+        li.appendChild(dateTimeNow);
+        li.appendChild(titleOfContent);
+        li.appendChild(bodyOfContent);
+        li.appendChild(btnDelete);
+        li.appendChild(btnEdit);
+
+        btnDelete.addEventListener("click", function () {
+            const parentArticleToBeDeleted = this.parentElement;
+            parentArticleToBeDeleted.style.display = "none";
+        })
+
+        btnEdit.addEventListener("click", function () {
+            const parentArticleToBeEdited = this.parentElement;
+            console.log(parentArticleToBeEdited);
+            console.log("dsss");
+            const editInputTitle=document.createElement("input");
+            editInputTitle.setAttribute("id","inputContentTitle");
+            const editInputContent=document.createElement("input");
+            editInputContent.setAttribute("id","inputContent");
+            const btnSave = document.createElement("button");
+            btnSave.setAttribute("id", "btnStyle");
+            btnSave.innerText = "Save";
+            parentArticleToBeEdited.appendChild(editInputTitle);
+            parentArticleToBeEdited.appendChild(editInputContent);
+            parentArticleToBeEdited.appendChild(btnSave);
+            const editTitle=parentArticleToBeEdited.children[1];
+            const editContent=parentArticleToBeEdited.children[2];
+            console.log(editTitle);
+            console.log("title above");
+            console.log(editContent);
+            console.log("editContent above");
+            editInputTitle.value=parentArticleToBeEdited.children[1].value;
+           editInputContent.value=parentArticleToBeEdited.children[2].value;
+
+        })
+
+    }
+}
+
+btnSubmitContent.addEventListener("click", function () {
+
+    const title = inputContentTitle.value;
+    const content = inputContent.value;
+    console.log(title);
+    console.log(content);
+    console.log("title and content above");
+    const articleOne = new Article(title, content);
+    articleOne.addArticle();
+    Clear();
 })
+
